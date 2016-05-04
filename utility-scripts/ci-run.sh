2bf654a8a194056a3d389ff04ab4194ffdc8dbc8
Jesper Oskarsson <jesosk@gmail.com>
2016-05-04 22:06:56 +0200

    Added CI scripts

---
+++ utility-scripts/ci-run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../bin/limbus-ci job hashicorp/precise64 ../utility-scripts/ci-test.sh
---
+++ utility-scripts/ci-test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get -y install git nodejs npm
+sudo ln -s /usr/local/bin/node /usr/local/bin/nodejs
+git clone https://github.com/redien/limbus-ci.git
+cd limbus-ci
+npm test
