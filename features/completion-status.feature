7c5f3acd53d254feefebaaf4da9fb85e7f1bfb75
Jesper Oskarsson <jesosk@gmail.com>
2016-05-04 12:47:17 +0200

    Started completion-status feature

--- features/completion-status.feature
+++ features/completion-status.feature
@@ -9,6 +9,12 @@
 # You should have received a copy of the CC0 Public Domain Dedication along with this software.
 # If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-/node_modules/
-/generated/
-/bin/
+Feature: Completion Status
+  As a user of limbus-ci
+  I want to run a job and have it return a completion status
+  So that I know if my job succeeded or not
+
+  Scenario: Running without an image
+    Given I do not supply an image to run
+    When I run a job
+    Then I should get a completion status of 'pending'
---
+++ features/step_definitions/build.js
@@ -0,0 +1,63 @@
+
+// limbus-ci - A simple CI solution for testing extremely portable applications.
+// Written in 2016 by Jesper Oskarsson jesosk@gmail.com
+//
+// To the extent possible under law, the author(s) have dedicated all copyright
+// and related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+var fs = require('fs');
+var child_process = require('child_process');
+
+var execute = function (command) {
+    return new Promise(function (resolve, reject) {
+        child_process.exec(command, function (error, stdout, stderr) {
+            if (error) {
+                reject(new Error('"' + command + '"' + ' exited with code ' + error.code + ':\n' + stdout + '\n' + stderr));
+            } else {
+                resolve({
+                    exitCode: 0,
+                    stdout: stdout,
+                    stderr: stderr
+                });
+            }
+        });
+    });
+};
+
+var parseResult = function (stdout) {
+    return JSON.parse(stdout);
+};
+
+module.exports = function () {
+    this.Given(/^I do not supply an image to run$/, function () {
+        this.image = '';
+        return Promise.resolve();
+    });
+
+    this.When(/^I run a job$/, function () {
+        var world = this;
+
+        var jobCommand = './bin/limbus-ci run job ' + world.image;
+
+        return execute(jobCommand).then(function (result) {
+            world.exitCode = result.exitCode;
+            world.stderr = result.stderr;
+            world.stdout = result.stdout;
+            world.result = parseResult(result.stdout);
+
+            return Promise.resolve();
+        });
+    });
+
+    this.Then(/^I should get a completion status of '(\w*)'$/, function (status) {
+        if (this.result.status === status) {
+            return Promise.resolve();
+        } else {
+            return Promise.reject(new Error('Expected completion status to be \'' + status + '\' but the job returned \'' + world.result.status + '\''));
+        }
+    });
+};
