42b1ad28cac54761b4b48f55ce300da6498909c1
Jesper Oskarsson <jesosk@gmail.com>
2016-05-07 00:09:10 +0200

    Fixed ln -s in ci-test.sh

--- utility-scripts/ci-test.sh
+++ utility-scripts/ci-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get -y install git nodejs npm
-sudo ln -s /usr/local/bin/node /usr/local/bin/nodejs
+sudo ln -s /usr/local/bin/nodejs /usr/local/bin/node
 git clone https://github.com/redien/limbus-ci.git
 cd limbus-ci
 npm test
