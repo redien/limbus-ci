6cf01ea391f54ea15105f1ff363d2cfa71271fc7
Jesper Oskarsson <jesosk@gmail.com>
2016-05-07 14:13:16 +0200

    Fixed sourcing of NVM

--- utility-scripts/ci-test.sh
+++ utility-scripts/ci-test.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 sudo apt-get -y install git vagrant virtualbox
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
-source /home/vagrant/.bashrc
+. ~/.nvm/nvm.sh # Load NVM into current shell session
 nvm install 0.10
 
 # Testing
