ea5c1aefe5049f549b3a27e40838f5d60438a279
Jesper Oskarsson <jesosk@gmail.com>
2016-05-07 12:27:28 +0200

    Added node 0.1.0 install in CI script

--- utility-scripts/ci-test.sh
+++ utility-scripts/ci-test.sh
@@ -5,6 +5,7 @@ sudo apt-get update
 sudo apt-get -y install git vagrant virtualbox
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 source /home/vagrant/.bashrc
+nvm install 0.10
 
 # Testing
 git clone https://github.com/redien/limbus-ci.git
